83621d4e837b58e8cfa8c8ff954f51f59960f96f
Dan Tull <dantull@gmail.com>
2025-05-26 11:25:41 -0500

    Spot check of board fill/unfill operations.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,19 @@ mod stringify;
 use stringify::convert_to_points;
 use stringify::convert_to_strings;
 
+fn print_board(points: &[Point], board: &board::Board<Point>) {
+    let board_strs = convert_to_strings(points, |p| {
+        if let Some(marker) = board.at(p) {
+            marker.chars().next().unwrap_or(' ')
+        } else {
+            '-'
+        }
+    });
+
+    println!("{}", board_strs.join("\n"));
+}
+
+
 fn main() {
     let shape = vec![
         "****",
@@ -30,5 +43,17 @@ fn main() {
     let result = strs.join("\n");
     println!("{}", result);
 
-    let mut board = make_point_board((points));
+    let mut board = make_point_board(points.clone());
+
+    let res = board.fill(vec![Point { x: 0, y: 0 }, Point { x: 0, y: 1 }], "X");
+
+    if (res.is_some()) {
+        println!("Board after placing 2 square X piece:");
+        print_board(&points, &board);
+
+        board.unfill(res.unwrap());
+
+        println!("Board after un-placing 2 square X piece:");
+    }
+    print_board(&points, &board);
 }
